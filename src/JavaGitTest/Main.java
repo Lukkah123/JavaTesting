9fdf82fb63f7bc4be437e426c2d11d719b4c73f0
Lukkah123 <h_lukka@hotmail.com>
2019-11-20 10:41:57 +0100

    version 2

--- src/JavaGitTest/Main.java
+++ src/JavaGitTest/Main.java
@@ -2,7 +2,7 @@ package JavaGitTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Spindelmannen kan hoppa högt");
+        System.out.println("Spindelmannen kan hoppa högt igen");
         Epix henke = new Epix("Henke", 35);
 
         System.out.println(henke);
