41f293e052c851b5e4e9a08fdf58ee7ce7fbeb71
unknown <fompe@hotmail.se>
2019-11-20 10:50:22 +0100

    Version 3

--- src/JavaGitTest/Main.java
+++ src/JavaGitTest/Main.java
@@ -3,6 +3,7 @@ package JavaGitTest;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Spindelmannen kan hoppa högt igen");
+        System.out.println("Spindelmannen är röd");
         Epix henke = new Epix("Henke", 35);
 
         System.out.println(henke);
