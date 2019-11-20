f4dd2231021a1a8264edada09299379d620bd414
HugSig <hugoh@hotmail.se>
2019-11-20 10:32:45 +0100

    Ändrad Version

--- src/JavaGitTest/Main.java
+++ src/JavaGitTest/Main.java
@@ -2,7 +2,7 @@ package JavaGitTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Jag har nu ändrat detta");
+        System.out.println("Spindelmannen kan hoppa högt");
         Epix henke = new Epix("Henke", 35);
 
         System.out.println(henke);
